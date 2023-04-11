347bf0e7b464f59521e840c84f4ef353b130dcdb
ondiano <130421828+ondiano@users.noreply.github.com>
2023-04-11 11:50:42 +0800

    Svelte basic calculator

---
+++ myapp/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+
+<h1>Basic Calculator</h1>
+
+<script>
+	let fnum = 0;
+	let snum = 0;
+	let sum = 0;
+
+	function add(){
+		return sum = fnum + snum;
+	}
+</script>
+
+<input type="number" bind:value = {fnum} placeholder="First number" />
+<input type="number" bind:value = {snum} placeholder="Second number "/>
+<button on:click = {add}>Add</button>
+
+<p>The sum is: {sum}</p>
